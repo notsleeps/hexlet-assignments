bc91f881a37cecfd44680b40aa21dbfd7a8b978d
@hexlet/cli <support@hexlet.io>
2023-05-10 21:42:20 +0300

    submit java-collections-ru/lists_10.05.23_21.40.34

---
+++ java-collections-ru/lists_10.05.23_21.40.34/src/main/java/exercise/App.java
@@ -0,0 +1,33 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+class App {
+ public static boolean scrabble(String symbols, String word) {
+        boolean answer = true;
+        symbols = symbols.toLowerCase();
+        word = word.toLowerCase();
+        List<Character> symbolList = new ArrayList<>();
+        List<Character> wordlList = new ArrayList<>();
+        char[] charSymbols = symbols.toCharArray();
+        char[] charWord = word.toCharArray();
+        for (char ch : charWord) {
+            wordlList.add(ch);
+        }
+        for (char ch : charSymbols) {
+            symbolList.add(ch);
+        }
+
+        for (Character c : wordlList) {
+            if (symbolList.contains(c)) {
+                symbolList.remove(c);
+            } else {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+//END
