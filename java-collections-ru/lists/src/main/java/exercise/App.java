4d567c6171c63b9e957ab33e8ac7e359fcf15b11
@hexlet/cli <support@hexlet.io>
2023-05-10 21:32:05 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -10,14 +10,19 @@ class App {
         symbols = symbols.toLowerCase();
         word = word.toLowerCase();
         List<Character> symbolList = new ArrayList<>();
+        List<Character> wordlList = new ArrayList<>();
         char[] charSymbols = symbols.toCharArray();
         char[] charWord = word.toCharArray();
+        for (char ch : charWord) {
+            wordlList.add(ch);
+        }
         for (char ch : charSymbols) {
             symbolList.add(ch);
         }
-        for (int i = 0; i < charWord.length - 1; i++) {
-            if (symbolList.contains(charWord[i])) {
-                symbolList.remove(i);
+
+        for (Character c : wordlList) {
+            if (symbolList.contains(c)) {
+                symbolList.remove(c);
             } else {
                 answer = false;
                 break;
@@ -25,5 +30,4 @@ class App {
         }
         return answer;
     }
-}
 //END
